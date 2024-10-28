0cfda15d7e6db36eb2ddde4213cc14943628d49c
Arturo <arturo_guerrero@ciencias.unam.mx>
2024-10-27 18:27:50 -0600

    front-end docker fixed

---
+++ front-end/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>¡Hola, Vue!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
