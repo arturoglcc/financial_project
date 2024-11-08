3501f257479c34e77df0f82e627a7ec6d453a25c
Mafer <mafer_rj@ciencias.unam.mx>
2024-11-07 21:39:46 -0600

    Add the rfc and curp to the user table of the database

--- backend/models.py
+++ backend/models.py
@@ -7,4 +7,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)  
     email = Column(String(255), unique=True, index=True, nullable=False)  
     username = Column(String(255), unique=True, index=True, nullable=False)  
-    password_hashed = Column(String(255), nullable=False)  
+    password_hashed = Column(String(255), nullable=False)
+    curp = Column(String(18), unique=True, nullable=True)
+    rfc = Column(String(13), unique=True, nullable=True)
