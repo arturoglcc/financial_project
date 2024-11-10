19b7c5d86a8ab36c06b83db0b2360d50dadd2a35
Mafer <mafer_rj@ciencias.unam.mx>
2024-11-09 21:47:20 -0600

    The start.sh file was created.

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
-.
+# start.sh
+#!/bin/bash
+source ~/mi_entorno/bin/activate  # Activa el entorno virtual
+alembic revision --autogenerate -m "Add curp, rfc, and nombre columns to users"  # Ejecuta el comando de Alembic
